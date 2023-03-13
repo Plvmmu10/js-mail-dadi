2b7780a803d317ccc7c3e1fb0a9361c2e9a96b4b
Plvmmu10 <gluca0260@gmail.com>
2023-03-13 16:00:11 +0100

    added email validation config

--- js/script.js
+++ js/script.js
@@ -6,3 +6,54 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+//Email section
+let verifiedEmailList = [
+    'giuseppeluca@gmail.com',
+    'cleliafradella@gmail.com',
+    'marcoacciarri@gmail.com',
+    'samuelpanicucci@gmail.com'
+]
+
+
+let emailBox = document.getElementById('email');
+
+const emailSubmit = document.getElementById('emailSubmit');
+// Email verification
+emailSubmit.addEventListener('click', function () {
+    
+    let emailValue = emailBox.value;
+    
+
+    for (i = 0; i < verifiedEmailList.length; i++){
+        
+    }
+
+
+    if (emailValue === verifiedEmailList[i]) {
+            
+        document.getElementById('verified-email').innerHTML = `
+        <h4 class="text-success">Email verificata!</h4>
+        `
+        
+    } else {
+        
+        document.getElementById('verified-email').innerHTML = `
+        <h4 class="text-danger">Email non verificata!</h4>`
+        
+    }
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
