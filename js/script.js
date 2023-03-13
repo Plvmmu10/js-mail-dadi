c3a2cda9f4a290dfbc112637061fe288c55c950b
Plvmmu10 <gluca0260@gmail.com>
2023-03-13 17:30:45 +0100

    added draw option in dice game config

--- js/script.js
+++ js/script.js
@@ -25,30 +25,30 @@ const emailSubmit = document.getElementById('emailSubmit');
 
 // Email verification
 emailSubmit.addEventListener('click', function () {
-    
+
     let emailValue = emailBox.value;
     let emailValid = false;
 
-    for (i = 0; i < verifiedEmailList.length; i++){
-        if (emailValue === verifiedEmailList[i]){
+    for (i = 0; i < verifiedEmailList.length; i++) {
+        if (emailValue === verifiedEmailList[i]) {
             emailValid = true;
         }
     }
 
 
     if (emailValid === true) {
-            
+
         document.getElementById('verified-email').innerHTML = `
         <h4 class="text-success">Email verificata!</h4>
         `
-        
+
     } else {
-        
+
         document.getElementById('verified-email').innerHTML = `
         <h4 class="text-danger">Email non verificata!</h4>`
-        
+
     }
-    
+
 })
 
 
@@ -57,23 +57,26 @@ emailSubmit.addEventListener('click', function () {
 
 
 
-const diceSubmit = document.getElementById ('diceSubmit');
+const diceSubmit = document.getElementById('diceSubmit');
 
 
 diceSubmit.addEventListener('click', function () {
 
-    let userDice = rndNumber(1,6);
-    let computerDice = rndNumber(1,6); 
+    let userDice = rndNumber(1, 6);
+    let computerDice = rndNumber(1, 6);
 
     document.getElementById('user-dice').innerHTML += `<h3 class="text-white">${userDice}</h3>`
     document.getElementById('computer-dice').innerHTML += `<h3 class="text-white">${computerDice}</h3>`
 
-    if (userDice > computerDice){
+    if (userDice > computerDice) {
         document.getElementById('winner').innerHTML = `<h3 class="text-success">Hai vinto!!</h3>`
-    }else{
+    } else if (userDice === computerDice){
+        document.getElementById('winner').innerHTML = `<h3>Hai pareggiato</h3>`
+    }
+    else {
         document.getElementById('winner').innerHTML = `<h3 class="text-danger">Hai perso..</h3>`
     }
-    
+
 
 })
 
